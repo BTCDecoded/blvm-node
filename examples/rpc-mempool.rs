4db23f81284b601fbf23f7681315b9543b4810ec
Zachary Johnson <zachary.business@tutanota.com>
2026-03-07 10:12:13 -0500

    minor spacing updates

--- examples/rpc-mempool.rs
+++ examples/rpc-mempool.rs
@@ -24,7 +24,7 @@ fn main() -> anyhow::Result<()> {
     println!();
 
     let rpc_url = "http://127.0.0.1:18332"; // Testnet
-                                                  // let rpc_url = "http://127.0.0.1:8332"; // Mainnet
+                                            // let rpc_url = "http://127.0.0.1:8332"; // Mainnet
 
     println!("RPC Endpoint: {rpc_url}");
     println!();
